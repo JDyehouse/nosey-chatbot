f65c4097c953fe8f25258eda28a6ebfcd97ba59d
JDyehouse <92692548+JDyehouse@users.noreply.github.com>
2022-03-27 09:38:09 +0100

    Add files via upload
    Rewrote code to make use of functions.

--- Nosey Chatbot.py
+++ Nosey Chatbot.py
@@ -1,10 +1,13 @@
 # Write your code here :-)
-age = 0
-print("What is your name?")
-name = input()
-while age == 0:
+def ask_name_and_age():
+    print("What is your name?")
+    name = input()
     print("Hello " + name + ". How old are you?")
     age = int(input())
+    bot_response(age)
+    return name
+
+def bot_response(age):
     if age >= 130:
         timelord_dict={"Timelord","timelord","Time Lord", "time lord"}
         vampire_dict={"Vampire", "Vampyre", "vampire", "vampyre"}
@@ -22,3 +25,6 @@ while age == 0:
         print("Ah, the joys of youth!")
     else:
         print("Ah, so you are " + str(age) + " years old!")
+    return
+
+ask_name_and_age()
